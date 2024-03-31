315a9364c8c148ce76fff984f13e1bd5e68adc5e
Null <cainiaohanhanyai@sina.com>
2024-03-31 14:38:31 +0800

    Fix some bugs

--- H2CO3/src/main/java/org/koishi/launcher/h2co3/ui/H2CO3MainActivity.java
+++ H2CO3/src/main/java/org/koishi/launcher/h2co3/ui/H2CO3MainActivity.java
@@ -4,6 +4,12 @@
  * //
  */
 
+/*
+ * //
+ * // Created by cainiaohh on 2024-03-31.
+ * //
+ */
+
 package org.koishi.launcher.h2co3.ui;
 
 import android.annotation.SuppressLint;
@@ -11,8 +17,6 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.PopupWindow;
-import android.widget.Toast;
 
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
@@ -25,11 +29,13 @@ import org.koishi.launcher.h2co3.R;
 import org.koishi.launcher.h2co3.resources.component.H2CO3ToolBar;
 import org.koishi.launcher.h2co3.resources.component.activity.H2CO3Activity;
 
+import java.util.Objects;
+
 public class H2CO3MainActivity extends H2CO3Activity implements View.OnClickListener {
-    public PopupWindow popView;
 
     private H2CO3ToolBar toolbar;
     private NavController navController;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -38,17 +44,17 @@ public class H2CO3MainActivity extends H2CO3Activity implements View.OnClickList
         initUI();
         NavigationRailView navView = findViewById(R.id.nav);
         NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
-        NavController navController = null;
         if (navHostFragment != null) {
             navController = navHostFragment.getNavController();
         }
+        toolbar.inflateMenu(R.menu.home_toolbar);
         setSupportActionBar(toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_directory, R.id.navigation_manage)
                 .build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(navView, navController);
-
+        NavigationUI.setupActionBarWithNavController(this, this.navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(navView, this.navController);
     }
 
     private void initUI() {
@@ -64,6 +70,7 @@ public class H2CO3MainActivity extends H2CO3Activity implements View.OnClickList
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.home_toolbar, menu);
         return true;
     }
@@ -72,11 +79,7 @@ public class H2CO3MainActivity extends H2CO3Activity implements View.OnClickList
     public boolean onOptionsItemSelected(MenuItem item) {
         int itemId = item.getItemId();
         if (itemId == R.id.action_item_home) {
-            Toast.makeText(this, "菜单项1被点击", Toast.LENGTH_SHORT).show();
-            return true;
         } else if (itemId == R.id.action_item_setting) {
-            Toast.makeText(this, "菜单项2被点击", Toast.LENGTH_SHORT).show();
-            return true;
         }
         return super.onOptionsItemSelected(item);
     }
